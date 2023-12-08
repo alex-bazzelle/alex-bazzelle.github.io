2100bfe40499dd286ba6acdd5112fb711f8ac817
Alex <58120299+alex-bazzelle@users.noreply.github.com>
2023-12-08 17:43:36 -0600

    Create colorChanger.js

---
+++ colorChanger.js
@@ -0,0 +1,22 @@
+// Get references to the circle and button elements
+const circle = document.getElementById('circle');
+const colorButton = document.getElementById('colorButton');
+
+// Function to generate a random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Function to change the color of the circle
+function changeColor() {
+    const newColor = getRandomColor();
+    circle.style.backgroundColor = newColor;
+}
+
+// Add a click event listener to the button
+colorButton.addEventListener('click', changeColor);
