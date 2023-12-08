98c3c0b2e7f4cc0a5352b8653de20ee936a5410d
Alex <58120299+alex-bazzelle@users.noreply.github.com>
2023-12-08 17:49:19 -0600

    Update colorChanger.js

--- colorChanger.js
+++ colorChanger.js
@@ -1,3 +1,8 @@
+// Example initialization code for your game
+        var gameContainer = document.getElementById('game-container');
+        // Initialize your game within the 'game-container'
+
+
 // Get references to the circle and button elements
 const circle = document.getElementById('circle');
 const colorButton = document.getElementById('colorButton');
